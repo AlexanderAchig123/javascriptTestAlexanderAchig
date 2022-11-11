d06cc03a4898c48729f2f2b124dc017f2f564a4c
AlexanderAchig123 <diminombrezura123@gmail.com>
2022-11-11 10:35:25 -0500

    a

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function mainFunction(){ 
+    let texto = document.getElementById("textoIngresado").innerHTML
+ 
+    alert ("Ups! Nada que mostrar")
+
+}
+function updateHeader(){
+
+}
+
+function consola(){
+    console.log("BIENVENIDO A LA EVALUACIÓN DE JAVASCRIPT!!!")
+}
